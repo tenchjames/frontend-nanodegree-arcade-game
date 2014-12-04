de56b26e303261b33496e3edf0743dd5cc57d0de
James Tench <tenchjames@gmail.com>
2014-12-04 09:19:46 -0500

    got the screen to render

--- js/app.js
+++ js/app.js
@@ -24,12 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-cat-girl.png';
+}
 
+// Player: can we rewrite some of this into common code?
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var enemy = new Enemy();
+allEnemies.push(enemy);
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
